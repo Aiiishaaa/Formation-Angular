5e2a4a61d296d8253403c6507d122ef194f642e8
Aiiishaaa <aicha.hamida06@yahoo.fr>
2022-07-05 17:07:18 +0200

    directives & binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Stagiaire } from './interfaces/stagiaire';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formation-angular';
+  tab: number[] = [2, 5, 3, 8];
+  stagiaire: Stagiaire = { num: 100, nom: "wick", prenom: "john" };
+  direBonjour() {
+    alert('Bonjour Angular !');
+  }
+  stagiaires : Stagiaire[] = [
+    { num: 100, nom: "wick", prenom: "john" },
+    { num: 200, nom: "dalton", prenom: "jack" },
+    { num: 300, nom: "doe", prenom: "joe" },
+    { num: 400, nom: "maggio", prenom: "sophie" },
+  ]
+
+  moyennes: number[] = [12, 5, 16, 18];
+
 }
---
+++ src/app/interfaces/stagiaire.ts
@@ -0,0 +1,5 @@
+export interface Stagiaire {
+    num?: number;
+    nom?: string;
+    prenom?: string;
+}
